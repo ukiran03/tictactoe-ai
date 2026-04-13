196a0f955b13e0d0b2daae65be3e8829c3089086
ukiran03 <ushakiranreddi@gmail.com>
2026-04-13 16:02:01 +0530

    refactor[board]: move printing logic to separate file

--- internal/board/board.go
+++ internal/board/board.go
@@ -1,11 +1,5 @@
 package board
 
-import (
-	"fmt"
-	"io"
-	"strings"
-)
-
 // CellState: represents the value of a single square
 type CellState int
 
@@ -14,17 +8,6 @@ const (
 	XState                    // 1
 	NilState                  // 2
 
-)
-
-var cellSymbols = map[CellState]string{
-	OState:   "O",
-	XState:   "X",
-	NilState: " ",
-}
-
-const (
-	Vbar = "│"
-	Hbar = "─"
 )
 
 type Board struct {
@@ -43,52 +26,4 @@ func NewBoard() *Board {
 		Grid:  grid,
 		Width: 3,
 	}
-}
-
-func (b Board) Render() string {
-	var out strings.Builder
-
-	// Boarders
-	top := NewBorder(topBorder)
-	mid := NewBorder(midBorder)
-	bot := NewBorder(botBorder)
-
-	// Vertical Num Row
-	writeHIndex(&out, 3, 1, 3)
-
-	top.render(&out, 3, 3) // Top border
-	for i, row := range b.Grid {
-		writeCellRow(&out, fmt.Sprintf("%d ", i), 1, row)
-
-		if i < len(b.Grid)-1 {
-			mid.render(&out, 3, 3) // Mid border
-		}
-	}
-	bot.render(&out, 3, 3) // Bot border
-	return out.String()
-}
-
-// Usage: writeCellRow(&w, 1, state)
-func writeCellRow(w io.Writer, prefix string, pad int, state [3]CellState) {
-	fmt.Fprint(w, prefix)
-	for _, cs := range state {
-		// %s is the Vbar
-		// %*s handles the padding + the symbol
-		fmt.Fprintf(w, "%s %*s ", Vbar, pad, cellSymbols[cs])
-	}
-	// Add the closing bar for the far right
-	fmt.Fprintf(w, "%s\n", Vbar)
-}
-
-// writeHIndex: writes Horizontal Index row
-// rng: 0, 1, 2,..
-// pad: padding length
-// prefix: prefix length for each integer
-// Usage: writeHIndex(&w, 3, 1, 3)
-func writeHIndex(w io.Writer, rng, pad, prefix int) {
-	fmt.Fprintf(w, "%*s", pad, "")
-	for i := range rng {
-		fmt.Fprintf(w, "%*s%d", prefix, "", i)
-	}
-	fmt.Fprint(w, "\n")
 }
---
+++ internal/board/print.go
@@ -0,0 +1,66 @@
+package board
+
+import (
+	"fmt"
+	"io"
+	"strings"
+)
+
+var cellSymbols = map[CellState]string{
+	OState:   "O",
+	XState:   "X",
+	NilState: " ",
+}
+
+const (
+	Vbar = "│"
+	Hbar = "─"
+)
+
+func (b Board) Render() string {
+	var out strings.Builder
+
+	// Boarders
+	top := NewBorder(topBorder)
+	mid := NewBorder(midBorder)
+	bot := NewBorder(botBorder)
+
+	// Vertical Num Row
+	writeHIndex(&out, 3, 1, 3)
+
+	top.render(&out, 3, 3) // Top border
+	for i, row := range b.Grid {
+		writeCellRow(&out, fmt.Sprintf("%d ", i), 1, row)
+
+		if i < len(b.Grid)-1 {
+			mid.render(&out, 3, 3) // Mid border
+		}
+	}
+	bot.render(&out, 3, 3) // Bot border
+	return out.String()
+}
+
+// Usage: writeCellRow(&w, 1, state)
+func writeCellRow(w io.Writer, prefix string, pad int, state [3]CellState) {
+	fmt.Fprint(w, prefix)
+	for _, cs := range state {
+		// %s is the Vbar
+		// %*s handles the padding + the symbol
+		fmt.Fprintf(w, "%s %*s ", Vbar, pad, cellSymbols[cs])
+	}
+	// Add the closing bar for the far right
+	fmt.Fprintf(w, "%s\n", Vbar)
+}
+
+// writeHIndex: writes Horizontal Index row
+// rng: 0, 1, 2,..
+// pad: padding length
+// prefix: prefix length for each integer
+// Usage: writeHIndex(&w, 3, 1, 3)
+func writeHIndex(w io.Writer, rng, pad, prefix int) {
+	fmt.Fprintf(w, "%*s", pad, "")
+	for i := range rng {
+		fmt.Fprintf(w, "%*s%d", prefix, "", i)
+	}
+	fmt.Fprint(w, "\n")
+}
